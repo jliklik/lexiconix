df5a67efb7ed2a59941e8e06bb690a9506b1f694
Jian Lik Ng <jlik.lik@gmail.com>
2026-02-26 10:05:46 -0800

    Can't call cargo build directly since it requires network access...

--- rust/default.nix
+++ rust/default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.stdenv.mkDerivation {
-  name = "libratatea";
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "libratatea";
+  version = "0.1.0";
  
   src = fetchGit {
     url = "https://github.com/jliklik/ratatea.git";
@@ -8,17 +9,7 @@ pkgs.stdenv.mkDerivation {
     rev = "126fac614cba020bdc675fe1ec6ee83ebdb395c1";
   };
 
-  nativeBuildInputs = [
-    pkgs.rustc
-    pkgs.cargo
-  ];
-
-  buildPhase = ''
-    cargo build --release
-  '';
-
-  installPhase = ''
-    sudo mkdir -p $out/bin
-    sudo cp target/release/libratatea.d $out/bin/
-  '';
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+  };
 }
